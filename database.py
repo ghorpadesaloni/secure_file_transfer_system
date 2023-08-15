8e6ae6a5f6e99c8a13025cb467d7dc9ea2c2534d
saloni <saloni02ghorpade@gmail.com>
2023-08-16 00:49:56 +0530

    phase1:registration and login process

---
+++ database.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+# Create the PostgreSQL database connection
+
+
+def get_db_connection():
+    return psycopg2.connect(
+        host='localhost',
+        database='secure_file_system',
+        user='postgres',
+        password='ruegen'
+    )
+
+# Create the users table if it doesn't exist
+
+
+def create_users_table():
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT NOT NULL,
+                totp_secret TEXT NOT NULL
+            )
+        ''')
+        conn.commit()
